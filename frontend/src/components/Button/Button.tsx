73c2907de8f5d05079ffb58da1598db854e321b4
¨Susheel Thapa¨ <susheelthapa90@gmail.com>
2024-01-19 17:46:17 +0545

    Create hero section

--- frontend/src/components/Button/Button.tsx
+++ frontend/src/components/Button/Button.tsx
@@ -2,13 +2,14 @@ import { ReactNode } from "react";
 
 interface Props {
   children: ReactNode;
+  className:string
 }
 
-const Button = ({ children }: Props) => {
+const Button = ({ children,className }: Props) => {
   return (
     <button
       type="button"
-      className="text-white bg-green-500 hover:bg-green-700 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+      className={className}
     >
       {children}
     </button>
