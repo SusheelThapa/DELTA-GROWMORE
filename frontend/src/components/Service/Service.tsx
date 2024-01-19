ee775e15257fb4c7b9caa0f880439b4825224954
¨Susheel Thapa¨ <susheelthapa90@gmail.com>
2024-01-19 19:55:34 +0545

    style, refactor: change the style of service section

--- frontend/src/components/Service/Service.tsx
+++ frontend/src/components/Service/Service.tsx
@@ -1,20 +1,48 @@
 import { FaRocket } from "react-icons/fa";
-import { FcSupport } from "react-icons/fc";
+import { HiSupport } from "react-icons/hi";
 import { AiOutlineTeam } from "react-icons/ai";
 
 const Service = () => {
+  const services = [
+    {
+      id: 1,
+      icon: (
+        <FaRocket className="my-auto mr-4 bg-green-300 rounded-full text-4xl p-2" />
+      ),
+      title: "Farming Community!",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpafugiat enim tempore!sit amet consectetur adipisicing elit. Culpafugiat enim tempore!",
+    },
+    {
+      id: 2,
+      icon: (
+        <HiSupport className="my-auto mr-4 rounded-full bg-green-300 text-black text-4xl p-1" />
+      ),
+      title: "24/7 Support",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpafugiat enim tempore!sit amet consectetur adipisicing elit. Culpafugiat enim tempore!",
+    },
+    {
+      id: 3,
+      icon: (
+        <AiOutlineTeam className="my-auto mr-4 bg-green-300 rounded-full text-4xl p-1" />
+      ),
+      title: "Dedicated Team",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpafugiat enim tempore!sit amet consectetur adipisicing elit. Culpafugiat enim tempore!",
+    },
+  ];
   return (
-    <div className="flex p-12">
-      <div className="left">
+    <div className="flex mx-80 space-x-16">
+      <div className="left flex justify-center items-center ">
         <img
           src="https://media.istockphoto.com/id/1029301814/photo/farmer-ploughing-field.jpg?s=612x612&w=0&k=20&c=DUfDbLJ_gpkdPJZu3Nu3_Y_wdB64MP1lAoNtus1ewXQ="
           alt=""
-          className=""
         />
       </div>
-      <div className="right p-4">
-        <div className="text-4xl font-bold mb-4">Why Chose US?</div>
-        <div className="text-zinc-400">
+      <div className="right p-4 flex space-y-6 flex-col">
+        <div className="text-4xl font-bold mb-4">Why Chose US</div>
+        <div className="text-zinc-400 mb-4">
           <div>
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus nemo
             aspernatur enim nulla dignissimos cupiditate adipisci ab aliquam rem
@@ -26,36 +54,20 @@ const Service = () => {
           </div>
         </div>
         <ul>
-          <li className="mt-4 mb-4 font-semibold">
-            <div className="flex">
-              <FaRocket className="my-auto mr-4 bg-green-300 rounded-full text-4xl p-1" />
-              <h4>Farming Community!</h4>
-            </div>
-            <div className="text-zinc-400">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa
-              fugiat enim tempore!
-            </div>
-          </li>
-          <li className="mb-4 font-semibold">
-            <div className="flex">
-              <FcSupport className="my-auto mr-4 rounded-full bg-green-300 text-4xl p-1" />
-              <h4>24/7 Support</h4>
-            </div>
-            <div className="text-zinc-400">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa
-              fugiat enim tempore!
-            </div>
-          </li>
-          <li className="mb-4 font-semibold">
-            <div className="flex">
-              <AiOutlineTeam className="my-auto mr-4 bg-green-300 rounded-full text-4xl p-1" />
-              <h4>Dedicated Team</h4>
-            </div>
-            <div className="text-zinc-400">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa
-              fugiat enim tempore!
-            </div>
-          </li>
+          {services.map(({ id, icon, title, description }) => {
+            return (
+              <li
+                key={id}
+                className="mb-5 font-semibold flex flex-col space-y-3"
+              >
+                <div className="flex space-x-4 justify-start items-center">
+                  {icon}
+                  <h4>{title}</h4>
+                </div>
+                <div className="text-gray-400">{description}</div>
+              </li>
+            );
+          })}
         </ul>
       </div>
     </div>
