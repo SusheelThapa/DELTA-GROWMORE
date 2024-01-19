f26dc8c9a473c44b30bbb492e37139fd37705f08
¨Susheel Thapa¨ <susheelthapa90@gmail.com>
2024-01-19 15:53:13 +0545

    setup frontend of the project
    use vite and typescript for frontend project and tailwind for css
    
    use axios for api call, react-icons for icon in project

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+const App = () => {
+  // This will contain all the pages of our project
+  const pages = [];
+  return (
+    <>
+      <Router>
+        <Routes>
+          {/* Here we will be write all the routes */}
+        </Routes>
+      </Router>
+    </>
+  );
+};
+
+export default App;
