eebe37e0fd9713ada7ea5624f78a90ce90b02104
Sandesh-Pyakurel <pyakurelsandesh14@gmail.com>
2024-01-20 20:31:03 +0545

    added more companies to connect

--- frontend/src/pages/Connect/Connect.tsx
+++ frontend/src/pages/Connect/Connect.tsx
@@ -19,13 +19,26 @@ const Connect = () => {
     tags: ["Agribusiness", "Innovation"],
     companyDesc: "GreenField AgroTech Group is a global leader in agribusiness, combining innovation and traditional values. Our integrated approach enhances productivity, ensuring a reliable supply chain for the agricultural community."
   },
+  {
+    img: "https://media.istockphoto.com/id/1327656453/vector/leaf-icon-logo-design-template-farm-icon-design-organic-symbol-vector-agriculture-icon.jpg?s=612x612&w=0&k=20&c=dsyBh9Elp85ADR2TCApuGOXfXimMSp97YG_rgbfqNI4=",
+    companyName: "Lorem Ipsum",
+    tags: ["Farming", "Community"],
+    companyDesc: "Lorem Ipsum is committed to fostering community through sustainable farming practices. We strive to provide fresh, locally grown produce while supporting local communities and economies."
+  },
+  {
+    img: "https://media.istockphoto.com/id/1263328016/vector/leaf-logo-design-vector-illustration-abstract-leaf-logo-vector-in-creative-design-concept.jpg?s=612x612&w=0&k=20&c=qkqLz8WzQmWnnQlpbMxRXCyAepumQ2nPqA27zkoDEHQ=",
+    companyName: "TechAgri Innovations",
+    tags: ["Technology", "Agribusiness"],
+    companyDesc: "TechAgri Innovations pioneers the integration of technology in agriculture. Our solutions aim to optimize farming processes, increase efficiency, and contribute to the overall advancement of the agribusiness sector."
+  },
+  
 ]
 
   return (
     <>
 <div className="flex flex-col items-center w-full px-10 bg-green-50">
     <div className="header w-full text-center text-5xl font-bold my-8">
-        Find the Best Company for Your Product
+        Best Company for Your Product
     </div>
     {/* <div className="flex justify-center">
         <Carousel slides={slides} interval={3000} />
