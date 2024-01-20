73e0ab21d91cf685a72982f8b8ffa179070feb47
Subash-Lamichhane <077bct081.subash@pcampus.edu.np>
2024-01-20 09:38:52 +0545

    hero section of finance page completed

--- frontend/src/pages/Finance/Finance.tsx
+++ frontend/src/pages/Finance/Finance.tsx
@@ -1,5 +1,18 @@
+import FinanceHero from "../../components/Finance/FinanceHero";
+
 const Finance = () => {
-  return <div>This is finance page</div>;
+  return (
+    <>
+      <div className="flex flex-col w-full">
+        <div className="header text-green-600 text-5xl font-bold w-full text-center my-10">GrowMore for Everyone</div>
+        <div>
+          <FinanceHero/>
+        </div>
+        
+      </div>
+
+    </>
+  );
 };
 
 export default Finance;
