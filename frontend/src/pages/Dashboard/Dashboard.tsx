8f0259e159d0c6e9dd6dafae65989acc49aa6c9a
SusheelThapa <susheelthapa90@gmail.com>
2024-01-20 17:11:21 +0545

    style: improve the style of create post

--- frontend/src/pages/Dashboard/Dashboard.tsx
+++ frontend/src/pages/Dashboard/Dashboard.tsx
@@ -1,12 +1,7 @@
 import Posts from "../../components/Posts";
 
 const Dashboard = () => {
-  return (
-    <div className="grid grid-cols-3">
-      <Posts className="col-span-2" />
-      <h1 className="bg-red-100">Some thing will surely goes here</h1>
-    </div>
-  );
+  return <Posts className="w-full" />;
 };
 
 export default Dashboard;
