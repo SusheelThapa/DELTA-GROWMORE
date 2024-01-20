fccdd14662538ca8b3223b09a7c4368784398fbf
Ujjwal Jha <ujjwaljha1225@gmail.com>
2024-01-20 20:11:15 +0545

    Comments for Landing page

--- frontend/src/pages/LandingPage/LandingPage.tsx
+++ frontend/src/pages/LandingPage/LandingPage.tsx
@@ -1,3 +1,4 @@
+// Importing necessary components from their respective paths
 import Box from "../../components/Box";
 import Feature from "../../components/Feature";
 import Footer from "../../components/Footer";
@@ -6,18 +7,33 @@ import NavBar from "../../components/NavBar";
 import Service from "../../components/Service";
 import Testimonial from "../../components/Testimonial";
 
+// Functional component representing the landing page
 const LandingPage = () => {
   return (
     <div>
+      {/* Navigation bar component */}
       <NavBar />
+
+      {/* Hero section component */}
       <Hero />
+
+      {/* Box component, which might contain additional information or features */}
       <Box />
+
+      {/* Service section component, showcasing various services or features */}
       <Service />
+
+      {/* Feature section component, highlighting key features or aspects */}
       <Feature />
+
+      {/* Testimonial section component, displaying user testimonials or reviews */}
       <Testimonial />
+
+      {/* Footer component, providing information or links at the bottom of the page */}
       <Footer />
     </div>
   );
 };
 
+// Exporting the LandingPage component as the default export
 export default LandingPage;
